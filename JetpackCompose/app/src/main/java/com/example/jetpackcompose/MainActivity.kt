d895a3155448f7a8a31c94d091910a6f8e1090ae
Farjana <farjanaakter431@gmail.com>
2023-06-16 22:06:26 +0600

    Changes

--- JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -3,16 +3,22 @@ package com.example.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
             JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting(message= "Happy Birthday, Moon",from = "From MOU")
                 }
             }
         }
@@ -30,12 +36,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(message: String,from: String ,modifier: Modifier = Modifier) {
     Surface(color = Color.Cyan) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp,10.dp)
-        )
+        Column (
+            verticalArrangement = Arrangement.Center,
+            modifier = modifier
+                ){
+            Text(
+                text = message,
+                fontSize = 100.sp,
+                lineHeight = 110.sp,
+                textAlign = TextAlign.Center
+            )
+
+        }
+        Column (verticalArrangement = Arrangement.Bottom,
+        modifier = modifier.padding(10.dp)){
+            Text(
+                text = from,
+                textDecoration = TextDecoration.Underline,
+                fontSize = 30.sp,
+                modifier = modifier
+                    .align(alignment = Alignment.End)
+
+            )
+        }
+
     }
 }
 
@@ -44,6 +70,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthDayCardPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        Greeting(message= "Happy Birthday, Moon",from = "From MOU")
     }
 }
