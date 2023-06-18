44e86ba42edd1b4ac7533ebd3d73375f733dcc99
Farjana <farjanaakter431@gmail.com>
2023-06-19 00:48:57 +0600

    Changes

--- JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ JetpackCompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -14,12 +14,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +31,8 @@ class MainActivity : ComponentActivity() {
             JetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting(message= "Happy Birthday, Moon",from = "From MOU")
+                 //   Greeting(message= "Happy Birthday, Moon",from = "From MOU")
+                    GreetingWithImg(message = "Happy BirthDay, Saurav", from = "From Mou", modifier = Modifier)
                 }
             }
         }
@@ -64,12 +68,47 @@ fun Greeting(message: String,from: String ,modifier: Modifier = Modifier) {
 
     }
 }
+@Composable
+fun GreetingWithImg(message: String,from: String,modifier: Modifier){
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        //Greeting(message= "Happy Birthday, Moon",from = "From MOU")
+    }
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            ){
+        Text(
+            text = message,
+            textAlign = TextAlign.Center,
+            fontSize = 100.sp,
+            lineHeight = 120.sp
+
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp,
+            modifier = modifier
+                .padding(8.dp)
+                .align(Alignment.End)
+        )
+    }
+
+}
+
 
 @Preview(showBackground = true,
         showSystemUi = true)
 @Composable
 fun BirthDayCardPreview() {
     JetpackComposeTheme {
-        Greeting(message= "Happy Birthday, Moon",from = "From MOU")
+        //Greeting(message= "Happy Birthday, Moon",from = "From MOU")
+        GreetingWithImg(message = "Happy BirthDay,Saurav", from = "From Mou", modifier = Modifier)
+
     }
 }
